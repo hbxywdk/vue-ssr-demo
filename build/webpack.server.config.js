54de50b0aebdac3d1b93e5394cdc8451962b5ad7
hbxywdk <364746078@qq.com>
2019-03-20 10:41:11 +0800

    Update webpack.server.config.js

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -16,9 +16,11 @@ module.exports = merge(base, {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"server"'
     }),
-    // 这是将服务器的整个输出
-    // 构建为单个 JSON 文件的插件。
-    // 默认文件名为 `vue-ssr-server-bundle.json`
+    /*
+    使用webpack按需代码分割的特性的时候（require.ensure或动态import）结果就是服务端bundle会包含很多分开的文件。
+    'vue-ssr-webpack-plugin'作用是将其打包为一个单独的JSON文件，这个文件可以传入到bundleRenderer中(server.js)，可以极大地简化了工作流。
+    默认文件名为 `vue-ssr-server-bundle.json`，也可以参数形式传入其他名称
+    */
     new VueSSRPlugin()
   ]
 })
